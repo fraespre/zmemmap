8adc433d96e25a0818d2a429da30c80835e3a44c
fraespre <fraespre@gmail.com>
2022-03-18 00:17:22 +0100

    house keeping

--- _console.bat
+++
@@ -1,6 +0,0 @@
-set INSTALL_DIR=C:\mio\soft\dev\js\node-12.6
-rem set PATH=%PATH%;%CD%\bin
-set PATH=%INSTALL_DIR%;%PATH%
-echo %PARTH%
-
-start cmd /k "echo NodeJS: & node -v & echo. & echo NPM: & npm -v"
--- cmd_binaries.bat
+++
@@ -1,5 +0,0 @@
-rem del .\bin\*.exe
-
-npm run package-win
-npm run package-linux
-rem npm run package-macos
